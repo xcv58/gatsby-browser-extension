e85d15ac5ba88ef67e5e275ba1942102576b3a67
xcv58 <i@xcv58.com>
2019-04-15 22:53:29 -0700

    A working example

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,23 @@
  */
 
 // You can delete this file if you're not using it
+const fs = require('fs')
+const path = require('path')
+
+exports.onPostBuild = async ({ reporter }, pluginOptions) => {
+  const activity = reporter.activityTimer(`Build manifest.json`)
+  activity.start()
+
+  const manifest = JSON.parse(fs.readFileSync('manifest.json'))
+
+  // TODO: generate content_security_policy for inline script, references:
+  // https://www.w3.org/TR/2015/CR-CSP2-20150721/#script-src-hash-usage
+  // https://report-uri.com/home/hash
+  //Write manifest
+  fs.writeFileSync(
+    path.join(`public`, `manifest.json`),
+    JSON.stringify(manifest)
+  )
+
+  activity.end()
+ }

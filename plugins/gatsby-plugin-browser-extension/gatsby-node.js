fc2b2f1e7d9c9693652c3eb38d9eda726c84d5f0
xcv58 <i@xcv58.com>
2019-04-16 23:00:22 -0700

    temp commit: direct hack to work

--- plugins/gatsby-plugin-browser-extension/gatsby-node.js
+++ plugins/gatsby-plugin-browser-extension/gatsby-node.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 const path = require('path')
 const cheerio = require('cheerio')
 const crypto = require('crypto');
+const WriteFilePlugin = require('write-file-webpack-plugin')
+
 
 const SHA_ALGO = 'sha512'
 
@@ -35,4 +37,41 @@ exports.onPostBuild = async ({ reporter }, pluginOptions) => {
   )
 
   activity.end()
+}
+
+exports.onCreateWebpackConfig = ({
+  stage, getConfig, rules, loaders, actions
+}) => {
+  console.log('onCreateWebpackConfig:', stage)
+  if (stage != 'develop') { return }
+  console.log(getConfig().output);
+  console.log(getConfig().entry);
+  const config = getConfig()
+  const { plugins, output, entry } = config
+  actions.replaceWebpackConfig({
+    ...config,
+    output: {
+      ...output,
+      path: path.join('/Users/xcv58/work/gatsby-browser-extension', 'public')
+    },
+    entry: {
+      ...entry,
+      commons: [
+        // 'webpack-dev-server/client?http://localhost:8000',
+        // 'webpack/hot/dev-server',
+        'event-source-polyfill',
+        // '/Users/xcv58/work/gatsby-browser-extension/node_modules/webpack-hot-middleware/client.js?path=/__webpack_hmr&reload=true&overlay=false',
+        '/Users/xcv58/work/gatsby-browser-extension/node_modules/webpack-hot-middleware/client.js?path=http://localhost:8000/__webpack_hmr&reload=true&overlay=false',
+        '/Users/xcv58/work/gatsby-browser-extension/.cache/app'
+      ]
+      // config.entry[entryName] = [
+      //   'webpack-dev-server/client?http://localhost:' + env.PORT,
+      //   'webpack/hot/dev-server'
+      // ].concat(config.entry[entryName])
+    },
+    plugins: [...plugins, new WriteFilePlugin()]
+  })
+
+  console.log('after:')
+  console.log(getConfig().output);
 }
